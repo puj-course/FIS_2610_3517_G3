d151da443a503b99d87ca65f004314525c6b2055
Santiago Martínez <sanmartin0125@gmail.com>
2026-03-12 19:05:59 -0500

    feat: Añadir repositorio routine (#126)

---
+++ scr/scrBackend/norafit/src/main/java/com/norafit/norafit/repositories/RoutineRepository.java
@@ -0,0 +1,8 @@
+package com.norafit.norafit.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.norafit.norafit.entities.Routine;
+
+public interface RoutineRepository extends JpaRepository<Routine, Integer> {
+
+}
