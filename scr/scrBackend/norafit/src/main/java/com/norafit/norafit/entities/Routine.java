07b4f5217a1c04739b72b27e193ef00f88aedfb5
Santiago Martínez <sanmartin0125@gmail.com>
2026-03-12 19:15:46 -0500

    fix: Cambio de int por Integer en el routineId para que sea consistente con la entidad y compatible con JPA (#126)

--- scr/scrBackend/norafit/src/main/java/com/norafit/norafit/entities/Routine.java
+++ scr/scrBackend/norafit/src/main/java/com/norafit/norafit/entities/Routine.java
@@ -3,39 +3,40 @@ package com.norafit.norafit.entities;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+
 import jakarta.persistence.*;
 
 @Entity
 @Table(name = "routines")
 public class Routine {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
-
     private Integer routineId;
+
     private String routineName;
+
     private float totalTimeSeconds;
+
     private LocalDate created_at;
+
     @Transient
     private List<Exercises> exercises = new ArrayList<>();
+
     @ManyToOne
     @JoinColumn(name = "user_id")
     private User user;
-
-    // Constructor vacio 
     public Routine() {
     }
-
-    //  Constructor sin lista
-    public Routine(int routineId, String routineName, float totalTimeSeconds, LocalDate created_at) {
+    public Routine(Integer routineId, String routineName, float totalTimeSeconds, LocalDate created_at) {
         this.routineId = routineId;
         this.routineName = routineName;
         this.totalTimeSeconds = totalTimeSeconds;
         this.created_at = created_at;
     }
 
-    //  Constructor completo
-    public Routine(int routineId, String routineName, float totalTimeSeconds, LocalDate created_at, List<Exercises> exercises) {
+    public Routine(Integer routineId, String routineName, float totalTimeSeconds, LocalDate created_at, List<Exercises> exercises) {
         this.routineId = routineId;
         this.routineName = routineName;
         this.totalTimeSeconds = totalTimeSeconds;
@@ -43,12 +44,11 @@ public class Routine {
         this.exercises = exercises;
     }
 
-    // getters y setters
-     public int getRoutineId() {
+    public Integer getRoutineId() {
         return routineId;
     }
 
-    public void setRoutineId(int routineId) {
+    public void setRoutineId(Integer routineId) {
         this.routineId = routineId;
     }
 
@@ -83,9 +83,11 @@ public class Routine {
     public void setExercises(List<Exercises> exercises) {
         this.exercises = exercises;
     }
+
     public User getUser() {
         return user;
     }
+
     public void setUser(User user) {
         this.user = user;
     }
