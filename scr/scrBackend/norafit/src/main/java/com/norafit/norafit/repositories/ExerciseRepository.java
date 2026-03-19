b1c1545493638298632b54b60afa6bca0d80656d
Santiago1213bv <156140910+Santiago1213bv@users.noreply.github.com>
2026-03-18 19:30:45 -0500

    Update ExerciseRepository.java (#119)

--- scr/scrBackend/norafit/src/main/java/com/norafit/norafit/repositories/ExerciseRepository.java
+++ scr/scrBackend/norafit/src/main/java/com/norafit/norafit/repositories/ExerciseRepository.java
@@ -1,1 +1,11 @@
+package com.norafit.norafit.repositories;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.norafit.norafit.entities.Exercises;
+
+public interface ExerciseRepository extends JpaRepository<Exercises, Integer> {
+    List<Exercises> findByRoutineId(Integer routineId);
+}
