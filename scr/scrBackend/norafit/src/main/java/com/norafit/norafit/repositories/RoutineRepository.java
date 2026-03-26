7f28ebd7343a1c3f6546e82af2ee30a51dbdbec0
Santiago Martínez <sanmartin0125@gmail.com>
2026-03-26 09:54:47 -0500

    Implementación método rename (#146)

--- scr/scrBackend/norafit/src/main/java/com/norafit/norafit/repositories/RoutineRepository.java
+++ scr/scrBackend/norafit/src/main/java/com/norafit/norafit/repositories/RoutineRepository.java
@@ -3,9 +3,17 @@ package com.norafit.norafit.repositories;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.norafit.norafit.entities.Routine;
 
 public interface RoutineRepository extends JpaRepository<Routine, Integer> {
+
     List<Routine> findByUserId(Integer userId);
+
+    @Modifying
+    @Query("UPDATE Routine r SET r.routineName = :newName WHERE r.routineId = :id")
+    void renameRoutine(@Param("id") Integer id, @Param("newName") String newName);
 }
