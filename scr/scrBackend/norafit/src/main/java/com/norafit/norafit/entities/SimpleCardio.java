7c89dc5d23cc4659d788a03aac574af514d543d8
AlvarezSS <alvarezs.s@javeriana.edu.co>
2026-03-30 12:50:45 -0500

    feat: crear clase SimpleCardio (#149, #148)

---
+++ scr/scrBackend/norafit/src/main/java/com/norafit/norafit/entities/SimpleCardio.java
@@ -0,0 +1,24 @@
+package com.norafit.norafit.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import java.sql.Time;
+
+@Entity
+@Table(name = "simple_cardio")
+  
+public class SimpleCardio extends CardioExercise {
+    @Column(name = "recorded_time")
+    private Time recordedTime;
+
+    @Column(name = "recorded_velocity")
+    private Float recordedVelocity;
+
+    @Column(name = "cardio_machine")
+    private String cardioMachine;
+
+    @Column(name = "recorded_incline")
+    private Float recordedIncline;
+
+}
