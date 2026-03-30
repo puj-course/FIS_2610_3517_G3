ae17338a3099cff4b133a3ab3d98583567cb6fee
AlvarezSS <alvarezs.s@javeriana.edu.co>
2026-03-30 12:19:37 -0500

    feat: crear clase StrenghtExercise (#148, #149)

---
+++ scr/scrBackend/norafit/src/main/java/com/norafit/norafit/entities/StrengthExercise.java
@@ -0,0 +1,14 @@
+package com.norafit.norafit.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "strength_exercises")
+public class StrengthExercise extends Exercises {
+
+    @Column(name = "has_weight")
+    private boolean hasWeight;
+
+}
