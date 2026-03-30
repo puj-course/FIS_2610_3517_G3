422a7c5ea9ffa8ac2e8b044b2d275d3d68b44b51
AlvarezSS <alvarezs.s@javeriana.edu.co>
2026-03-30 13:02:27 -0500

    feat: constructores clase StrengthExercise

--- scr/scrBackend/norafit/src/main/java/com/norafit/norafit/entities/StrengthExercise.java
+++ scr/scrBackend/norafit/src/main/java/com/norafit/norafit/entities/StrengthExercise.java
@@ -6,9 +6,17 @@ import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "strength_exercises")
+    
 public class StrengthExercise extends Exercises {
 
     @Column(name = "has_weight")
     private boolean hasWeight;
 
+    //constructores
+    public StrengthExercise() {
+    }
+
+    public StrengthExercise(boolean hasWeight) {
+        this.hasWeight = hasWeight;
+    }
 }
