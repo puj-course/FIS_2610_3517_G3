0aee474e5d8913c2a8ce4725a1fa2eaff51313ed
Santiago1213bv <156140910+Santiago1213bv@users.noreply.github.com>
2026-04-05 12:06:28 -0500

    Update StrengthExercise.java (#180)

--- scr/scrBackend/norafit/src/main/java/com/norafit/norafit/entities/StrengthExercise.java
+++ scr/scrBackend/norafit/src/main/java/com/norafit/norafit/entities/StrengthExercise.java
@@ -1,30 +1,43 @@
 package com.norafit.norafit.entities;
 
-import jakarta.persistence.Column;
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.PrimaryKeyJoinColumn;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "strength_exercises")
-    
-public class StrengthExercise extends Exercises {
+@PrimaryKeyJoinColumn(name = "exercise_id") // Une esta tabla con la de Exercise
+public class StrengthExercise extends Exercise {
 
-    @Column(name = "has_weight")
     private boolean hasWeight;
 
-    //constructores
+    // Relación con StrengthSeries (1 a muchos)
+    @OneToMany(mappedBy = "strengthExercise", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<StrengthSeries> series = new ArrayList<>();
+
+    // Constructor vacío para JPA
     public StrengthExercise() {
+        super();
     }
 
-    public StrengthExercise(boolean hasWeight) {
-        this.hasWeight = hasWeight;
+    public List<StrengthSeries> getSeries() {
+        return series;
     }
 
-    //getters y setters
-    public boolean isHasWeight() {
-        return hasWeight;
+    // --- GETTERS Y SETTERS BÁSICOS ---
+    public boolean isHasWeight() { 
+        return hasWeight; 
     }
-    public void setHasWeight(boolean hasWeight) {
-        this.hasWeight = hasWeight;
+    public void setHasWeight(boolean hasWeight) { 
+        this.hasWeight = hasWeight; 
+    }
+    public void setSeries(List<StrengthSeries> series) { 
+        this.series = series; 
     }
 }
+
