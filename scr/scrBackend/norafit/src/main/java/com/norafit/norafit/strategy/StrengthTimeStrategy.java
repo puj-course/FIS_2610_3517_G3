b15bf23b36d7a3c9821382b386ee10d13006a6bd
Santiago Martínez <sanmartin0125@gmail.com>
2026-04-07 21:20:43 -0500

    correcciones

--- scr/scrBackend/norafit/src/main/java/com/norafit/norafit/strategy/StrengthTimeStrategy.java
+++ scr/scrBackend/norafit/src/main/java/com/norafit/norafit/strategy/StrengthTimeStrategy.java
@@ -8,12 +8,20 @@ public class StrengthTimeStrategy implements TimeCalculationStrategy {
 
     @Override
     public int calculateTime(Exercise exercise) {
-        StrengthExercise ex = (StrengthExercise) exercise;
+
+        StrengthExercise strength = (StrengthExercise) exercise;
 
         int total = 0;
 
-        for (StrengthSeries s : ex.getSeries()) {
-            total += s.getRepetitions() * s.getSecondsPerRep();
+        for (StrengthSeries s : strength.getSeries()) {
+
+            // Tiempo por rep (asumimos 2 segundos por repetición)
+            int repsTime = s.getRepetitions() * 2;
+
+            // Tiempo de descanso
+            int restTime = s.getRestTimeSeconds();
+
+            total += repsTime + restTime;
         }
 
         return total;
