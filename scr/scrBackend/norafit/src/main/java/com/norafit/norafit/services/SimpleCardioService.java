e85608e11b05670574504229a6f296d3a25446cd
AlvarezSS <alvarezs.s@javeriana.edu.co>
2026-04-19 22:27:21 -0500

    feat: crear clase SimpleCardioService (#250)

---
+++ scr/scrBackend/norafit/src/main/java/com/norafit/norafit/services/SimpleCardioService.java
@@ -0,0 +1,9 @@
+package com.norafit.norafit.services;
+
+import org.springframework.stereotype.Service;
+
+import com.norafit.norafit.entities.SimpleCardio;
+
+@Service
+public class SimpleCardioService {
+}
