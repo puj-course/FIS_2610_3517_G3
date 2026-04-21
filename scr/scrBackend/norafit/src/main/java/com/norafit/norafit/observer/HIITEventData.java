4298d708d9bc206840d2b3c4d4f31eaab13ed17b
AlvarezSS <alvarezs.s@javeriana.edu.co>
2026-04-20 19:10:59 -0500

    feat: crear clase HIITEventData (#282)

---
+++ scr/scrBackend/norafit/src/main/java/com/norafit/norafit/observer/HIITEventData.java
@@ -0,0 +1,11 @@
+package com.norafit.norafit.observer;
+
+public class HIITEventData {
+
+    private final HIITEvent event;
+    private final String message;
+    private final String exerciseName;
+    private final int round;
+    private final int totalRounds;
+    private final int secondsRemaining;
+}
