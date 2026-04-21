912c78a860420830dd2e6bb17091f5ba7fb2453c
AlvarezSS <alvarezs.s@javeriana.edu.co>
2026-04-20 19:43:02 -0500

    feat: crear logger en LogObserver (#282)

--- scr/scrBackend/norafit/src/main/java/com/norafit/norafit/observer/LogObserver.java
+++ scr/scrBackend/norafit/src/main/java/com/norafit/norafit/observer/LogObserver.java
@@ -6,4 +6,24 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class LogObserver implements IHIITObserver {
+
+    private static final Logger log = LoggerFactory.getLogger(LogObserver.class);
+
+    @Override
+    public void onEvent(HIITEventData data) {
+        switch (data.getEvent()) {
+            case ROUTINE_STARTED, ROUTINE_FINISHED,
+                 EXERCISE_STARTED, EXERCISE_FINISHED,
+                 ROUND_STARTED, WORK_FINISHED, REST_FINISHED ->
+                    log.info("[HIIT][{}] {}", data.getEvent(), data.getMessage());
+
+            case WORK_TICK, REST_TICK ->
+                    log.debug("[HIIT][{}] ejercicio='{}' ronda={}/{} segundos={}",
+                            data.getEvent(),
+                            data.getExerciseName(),
+                            data.getRound(),
+                            data.getTotalRounds(),
+                            data.getSecondsRemaining());
+        }
+    }
 }
