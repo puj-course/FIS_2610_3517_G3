d59794ad81633fd8b037a83144d82351909e128d
AlvarezSS <alvarezs.s@javeriana.edu.co>
2026-04-20 19:05:23 -0500

    feat: implementar HIITEvent (#282)

---
+++ scr/scrBackend/norafit/src/main/java/com/norafit/norafit/observer/HIITEvent.java
@@ -0,0 +1,13 @@
+package com.norafit.norafit.observer;
+
+public enum HIITEvent {
+    ROUTINE_STARTED,
+    ROUTINE_FINISHED,
+    EXERCISE_STARTED,
+    EXERCISE_FINISHED,
+    ROUND_STARTED,
+    WORK_TICK,
+    REST_TICK,
+    WORK_FINISHED,
+    REST_FINISHED
+}
