816e8d50c49d1e41d81de701c7c264f0e40eb6ea
AlvarezSS <alvarezs.s@javeriana.edu.co>
2026-04-20 19:30:46 -0500

    feat: implementar onEvent en ConsoleObserver (#282)

--- scr/scrBackend/norafit/src/main/java/com/norafit/norafit/observer/ConsoleObserver.java
+++ scr/scrBackend/norafit/src/main/java/com/norafit/norafit/observer/ConsoleObserver.java
@@ -4,4 +4,18 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class ConsoleObserver implements IHIITObserver {
+
+    @Override
+    public void onEvent(HIITEventData data) {
+      
+        switch (data.getEvent()) {
+            case EXERCISE_STARTED  -> System.out.println(data.getMessage());
+            case EXERCISE_FINISHED -> System.out.println(data.getMessage());
+            case ROUND_STARTED     -> System.out.println(data.getMessage());
+            case WORK_TICK         -> System.out.println("TRABAJO - " + data.getSecondsRemaining() + "s restantes");
+            case REST_TICK         -> System.out.println("DESCANSO - " + data.getSecondsRemaining() + "s restantes");
+            case WORK_FINISHED     -> System.out.println(data.getMessage());
+            case REST_FINISHED     -> System.out.println(data.getMessage());
+        }
+    }
 }
