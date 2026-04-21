a06c48f13301672befcbae14ee5e5dec4a453a26
AlvarezSS <alvarezs.s@javeriana.edu.co>
2026-04-20 19:35:17 -0500

    feat: crear clase LogObserver (#282)

---
+++ scr/scrBackend/norafit/src/main/java/com/norafit/norafit/observer/LogObserver.java
@@ -0,0 +1,9 @@
+package com.norafit.norafit.observer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+public class LogObserver implements IHIITObserver {
+}
