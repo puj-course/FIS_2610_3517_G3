4d8be99106617b91d44fece340c5160c65cc1451
Juan Mendoza <mendozajuanjose348@gmail.com>
2026-05-11 17:40:21 -0500

    Add console profile for CommandLineRunner

--- scr/scrBackend/norafit/src/main/java/com/norafit/norafit/NorafitApplication.java
+++ scr/scrBackend/norafit/src/main/java/com/norafit/norafit/NorafitApplication.java
@@ -1,20 +1,19 @@
 package com.norafit.norafit;
-
 import com.norafit.norafit.console.consoleApp;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
 public class NorafitApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(NorafitApplication.class, args);
     }
 
     @Bean
+    @Profile("console")
     CommandLineRunner run(consoleApp consoleApp) {
         return args -> consoleApp.start();
     }
