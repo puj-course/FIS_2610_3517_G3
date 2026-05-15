8559b77103bed42943b3deeb6688862e994851bd
Santiago1213bv <156140910+Santiago1213bv@users.noreply.github.com>
2026-05-15 13:11:31 -0500

    feat: Update RoutineController.java (#376)

--- scr/scrBackend/norafit/src/main/java/com/norafit/norafit/controllers/RoutineController.java
+++ scr/scrBackend/norafit/src/main/java/com/norafit/norafit/controllers/RoutineController.java
@@ -3,7 +3,6 @@ package com.norafit.norafit.controllers;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -31,7 +30,7 @@ public class RoutineController {
         this.routineFacade = routineFacade;
         this.userRepository = userRepository;
     }
-    @Transactional
+    
     public RoutineResponse toResponse(Routine routine) {
         List<RoutineResponse.ExerciseInfo> exercises = routine.getExercises()
             .stream()
