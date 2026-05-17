f1feddd3224039ddc9e7d9927d3f13f010b0d0dd
AlvarezSS <alvarezs.s@javeriana.edu.co>
2026-05-17 16:25:05 -0500

    feat: Update UserRepository.java (#364)

--- scr/scrBackend/norafit/src/main/java/com/norafit/norafit/repositories/UserRepository.java
+++ scr/scrBackend/norafit/src/main/java/com/norafit/norafit/repositories/UserRepository.java
@@ -1,11 +1,17 @@
 package com.norafit.norafit.repositories;
-import java.util.Optional;
-import com.norafit.norafit.entities.User;
-import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.norafit.norafit.entities.User;
+
+@Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
     Optional<User> findByEmail(String email);
     boolean existsByEmail(String email);
+    Optional<User> findByPhoneNumber(String phoneNumber);
 }
 
 //Estas interfaces ayudan a comunicarse con la BD porque extiende User que es la entidad y la llave Integer 
